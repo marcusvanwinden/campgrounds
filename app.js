3406515bba5439b3343093821e3b11062527345d
Marcus van Winden <marcus.van.winden@icloud.com>
2021-03-02 15:15:21 +0100

    Add route to create reviews

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
+const Review = require('./models/review');
 
 mongoose.connect('mongodb://localhost:27017/campgrounds', {
   useNewUrlParser: true,
@@ -100,6 +101,15 @@ app.delete(
     res.redirect('/campgrounds');
   })
 );
+
+app.post('/campgrounds/:id/reviews', async (req, res) => {
+  const campground = await Campground.findById(req.params.id);
+  const review = new Review(req.body.review);
+  campground.reviews.push(review);
+  await review.save();
+  await campground.save();
+  res.redirect(`/campgrounds/${campground._id}`);
+});
 
 app.all('*', (req, res, next) => {
   next(new ExpressError('Page not found', 404));
