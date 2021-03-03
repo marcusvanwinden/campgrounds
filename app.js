c6c5aea6e31ef67c46a82b18055d56ea7a936c02
Marcus van Winden <31445379+marcusvanwinden@users.noreply.github.com>
2021-03-03 10:03:32 +0100

    feat: Add reviews functionality ✨ (#4)

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
-const { campgroundSchema } = require('./schemas');
+const { campgroundSchema, reviewSchema } = require('./schemas');
 const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
+const Review = require('./models/review');
 
 mongoose.connect('mongodb://localhost:27017/campgrounds', {
   useNewUrlParser: true,
@@ -68,7 +69,9 @@ app.post(
 app.get(
   '/campgrounds/:id',
   catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id);
+    const campground = await Campground.findById(req.params.id).populate(
+      'reviews'
+    );
     res.render('campgrounds/show', { campground });
   })
 );
@@ -100,6 +103,32 @@ app.delete(
     res.redirect('/campgrounds');
   })
 );
+
+function validateReview(req, res, next) {
+  const { error } = reviewSchema.validate(req.body);
+  if (error) {
+    const message = error.details.map((element) => element.message).join(', ');
+    throw new ExpressError(message, 400);
+  } else {
+    next();
+  }
+}
+
+app.post('/campgrounds/:id/reviews', validateReview, async (req, res) => {
+  const campground = await Campground.findById(req.params.id);
+  const review = new Review(req.body.review);
+  campground.reviews.push(review);
+  await review.save();
+  await campground.save();
+  res.redirect(`/campgrounds/${campground._id}`);
+});
+
+app.delete('/campgrounds/:id/reviews/:reviewId', async (req, res) => {
+  const { id, reviewId } = req.params;
+  await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+  await Review.findByIdAndDelete(reviewId);
+  res.redirect(`/campgrounds/${id}`);
+});
 
 app.all('*', (req, res, next) => {
   next(new ExpressError('Page not found', 404));
