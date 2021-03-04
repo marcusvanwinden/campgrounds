6e24771dd42429206aacad9b9a7f414e08f9ed06
Marcus van Winden <marcus.van.winden@icloud.com>
2021-03-04 16:12:03 +0100

    Implement campground and review authorization

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,21 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const catchAsync = require('../utils/catchAsync');
-const ExpressError = require('../utils/ExpressError');
 const Campground = require('../models/campground');
-const { campgroundSchema } = require('../schemas');
-const { isLoggedIn } = require('../middleware');
-
-function validateCampground(req, res, next) {
-  const { error } = campgroundSchema.validate(req.body);
-
-  if (error) {
-    const message = error.details.map((element) => element.message).join(', ');
-    throw new ExpressError(message, 400);
-  } else {
-    next();
-  }
-}
+const { isLoggedIn, isAuthor, validateCampground } = require('../middleware');
 
 router.get(
   '/',
@@ -46,7 +33,12 @@ router.get(
   '/:id',
   catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id)
-      .populate('reviews')
+      .populate({
+        path: 'reviews',
+        populate: {
+          path: 'author',
+        },
+      })
       .populate('author');
     console.log(campground);
     if (!campground) {
@@ -60,6 +52,7 @@ router.get(
 router.get(
   '/:id/edit',
   isLoggedIn,
+  isAuthor,
   catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
     if (!campground) {
@@ -73,6 +66,7 @@ router.get(
 router.put(
   '/:id',
   isLoggedIn,
+  isAuthor,
   validateCampground,
   catchAsync(async (req, res) => {
     const campground = await Campground.findByIdAndUpdate(
@@ -87,6 +81,7 @@ router.put(
 router.delete(
   '/:id',
   isLoggedIn,
+  isAuthor,
   catchAsync(async (req, res) => {
     await Campground.findByIdAndDelete(req.params.id);
     req.flash('success', 'Successfully deleted campground');
--- seeds/index.js
+++ seeds/index.js
@@ -28,7 +28,7 @@ async function seedDB() {
     const randomCity = randomChoice(cities);
     const randomPrice = Math.floor(Math.random() * 20) + 10;
     const campground = await new Campground({
-      author: '6040a5285f5e88d9479c9a9d',
+      author: '6040d5c47cb4aae52696833c',
       location: `${randomCity.city}, ${randomCity.state}`,
       title: `${randomChoice(descriptors)} ${randomChoice(places)}`,
       image: `https://source.unsplash.com/collection/483251`,
