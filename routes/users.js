1051d25904123eccb0897aaddf5bb105f5952df1
Marcus van Winden <marcus.van.winden@icloud.com>
2021-03-04 19:57:50 +0100

    Seperate user logic in controller file

--- routes/users.js
+++ routes/users.js
@@ -1,35 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/user');
 const catchAsync = require('../utils/catchAsync');
 const passport = require('passport');
+const users = require('../controllers/users');
 
-router.get('/register', (req, res) => {
-  res.render('users/register');
-});
+router.get('/register', users.renderRegister);
 
-router.post(
-  '/register',
-  catchAsync(async (req, res, next) => {
-    try {
-      const { email, username, password } = req.body;
-      const user = new User({ email, username });
-      const registererdUser = await User.register(user, password);
-      req.login(registererdUser, (error) => {
-        if (error) return next(error);
-        req.flash('success', `Welcome, ${registererdUser.username}`);
-        res.redirect('/campgrounds');
-      });
-    } catch (error) {
-      req.flash('error', error.message);
-      res.redirect('/register');
-    }
-  })
-);
+router.post('/register', catchAsync(users.register));
 
-router.get('/login', (req, res) => {
-  res.render('users/login');
-});
+router.get('/login', users.renderLogin);
 
 router.post(
   '/login',
@@ -37,18 +16,9 @@ router.post(
     failureFlash: true,
     failureRedirect: '/login',
   }),
-  (req, res) => {
-    const redirectUrl = req.session.returnTo || '/campgrounds';
-    req.flash('success', 'Welcome back');
-    delete req.session.returnTo;
-    res.redirect(redirectUrl);
-  }
+  users.login
 );
 
-router.get('/logout', (req, res) => {
-  req.logout();
-  req.flash('success', 'Goodbye');
-  res.redirect('/campgrounds');
-});
+router.get('/logout', users.logout);
 
 module.exports = router;
