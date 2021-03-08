176a7664e99678c35b09c2188528e4e6a430eff3
Marcus van Winden <31445379+marcusvanwinden@users.noreply.github.com>
2021-03-08 15:23:53 +0100

    style: Add styling to most pages 💎 (#16)

--- public/js/showPageMap.js
+++ public/js/showPageMap.js
@@ -1,10 +1,13 @@
 mapboxgl.accessToken = mapToken;
+
 const map = new mapboxgl.Map({
   container: 'map',
   style: 'mapbox://styles/mapbox/streets-v11',
   center: campground.geometry.coordinates,
   zoom: 10,
 });
+
+map.addControl(new mapboxgl.NavigationControl());
 
 new mapboxgl.Marker()
   .setLngLat(campground.geometry.coordinates)
